a095018a664c395d883b16dcd9df670a6fa43a8a
EduardoAnanias05 <edujujunior@icloud.com>
2024-06-06 16:31:54 -0300

    commit

--- Nova pasta/functions.php
+++ Nova pasta/functions.php
@@ -11,37 +11,32 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
 
-        <body> 
+    <body> 
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-        <img id="logo" src="/img/SENAI_São_Paulo_logo.png" alt="Logo">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse"> 
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="login.php">Registra <span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-        </div>
+        <div class="navnav"> 
+            <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+                <img id="logo" src="/img/SENAI_São_Paulo_logo-removebg-preview.png" alt="Logo">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse"> 
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="login.php">Registra <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
+        </div>
+
 
             <div class="nav2">
-                <a href="cadastroFuncionario.php">Alugar</a>
-                <a href="#catalago">Catálago</a>
-                <a href="">Contato</a>
-                <a href="">Sobre</a>
             </div>
-
-            
-
-        </body>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
 EOT;
 }
--- Nova pasta/index.php
+++ Nova pasta/index.php
@@ -10,51 +10,81 @@ include 'functions.php';
 <?= template_header('LocEdu') ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4 mb-3 mb-sm-0">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Registre os Clientes</h5>
-                    <p class="card-text">Para Registrar os novos clientes aperte Aqui.</p>
-                    <a href="readClientes.php" class="btn btn-dark">LISTAR clientes</a>
-                    <a href="cliente.php" class="btn btn-dark">ADD clientes</a>
+    <section>
+
+
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img id="carousel-img" src="img/Análise de dados dashboard empresa vermelho e roxo.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img id="carousel-img" src="img/1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img id="carousel-img" src="img/2.png" class="d-block w-100" alt="...">
                 </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Gerencie os Automoveis</h5>
-                    <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
-                    <a href="createCarro.php" class="btn btn-danger">Registrar</a>
-                    <a href="readCarros.php" class="btn btn-danger">Listar</a>
+
+    </section>
+
+    <div class="row2">
+        <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <img src="img/icon-fabricacao.svg" class="card-img-top" alt="...">
+                        <h5 class="card-title">BLOCO A</h5>
+                        <p class="card-text">Para Registrar os novos clientes aperte Aqui.</p>
+                        <a href="readClientes.php" class="btn btn-outline-dark">LISTAR clientes</a>
+                        <a href="cliente.php" class="btn btn-outline-dark">ADD clientes</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div> 
-    <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Gerencie os Automoveis</h5>
-                    <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
-                    <a href="createCarro.php" class="btn btn-danger">Registrar</a>
-                    <a href="readCarros.php" class="btn btn-danger">Listar</a>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body"> 
+                    <img src="img/icon-tecnologia.svg" class="card-img-top" alt="...">
+                        <h5 class="card-title">BLOCO B</h5>
+                        <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
+                        <a href="createCarro.php" class="btn btn-outline-danger">Registrar</a>
+                        <a href="readCarros.php" class="btn btn-outline-danger">Listar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body"> 
+                    <img src="img/icon-administracao.svg" class="card-img-top" alt="...">
+                        <h5 class="card-title">BLOCO C</h5>
+                        <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
+                        <a href="createCarro.php" class="btn btn-outline-danger">Registrar</a>
+                        <a href="readCarros.php" class="btn btn-outline-danger">Listar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body"> 
+                    <img src="img/icon-eletronica.svg" class="card-img-top" alt="...">
+                        <h5 class="card-title">BLOCO D</h5>
+                        <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
+                        <a href="createCarro.php" class="btn btn-outline-dark">Registrar</a>
+                        <a href="readCarros.php" class="btn btn-outline-dark">Listar</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Gerencie os Automoveis</h5>
-                    <p class="card-text">Aqui Voce pode gerenciar os Veiculos.</p>
-                    <a href="createCarro.php" class="btn btn-dark">Registrar</a>
-                    <a href="readCarros.php" class="btn btn-dark">Listar</a>
-                </div>
-            </div>
-        </div>
-    </div> 
-    
- 
+
     <?= template_footer() ?>
 </div>
